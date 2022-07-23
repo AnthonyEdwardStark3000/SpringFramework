3db33ef4c62c3dfa15e41c6314ce69fd67a72f88
AnthonyEdwardStark3000 <catchmesureshbabu8@gmail.com>
2022-07-23 22:43:35 +0530

    Simple hello world MVC

---
+++ springmvc/src/main/java/com/Mr_stark/spring/springmvc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.Mr_stark.spring.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller // to make spring understand that it can create bean for handling Http req
+public class HelloController {
+	@RequestMapping("/hello") //telling spring to invoke this method while getting this http request
+	public ModelAndView hello() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("hello"); // contains the name of the view file . Here it is hello.jsp
+		return modelAndView;
+	}
+}
